b42edae0b4c4e3df0c5271ae055ad09fd4a5848b
iftekher2108 <parvezkhan21082002@gmail.com>
2023-02-06 22:52:50 +0600

    gallery and skill content added

--- custom/js/main.js
+++ custom/js/main.js
@@ -1,1 +1,22 @@
 console.log("iftekher mahmud pervez")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// footer dynamic date set
+let target =document.querySelector("body > footer > div > p > span");
+let date = new Date();
+let year = date.getFullYear();
+target.innerHTML=year;
+// footer dynamic date set
